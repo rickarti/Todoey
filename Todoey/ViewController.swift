39046120c384b569355d6ed3413031fda27a75d7
Richard Johnson <rickarti@sbcglobal.net>
2019-09-03 17:46:50 -0700

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Richard Johnson on 9/3/19.
+//  Copyright © 2019 Richard Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
